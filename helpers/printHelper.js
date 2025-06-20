d3e9a5cac47ccfad4a58397318cf57386634e022
Alexandr <muhabon93@gmail.com>
2025-06-20 16:47:19 +0300

    Cleanup nested repo, unify project

---
+++ helpers/printHelper.js
@@ -0,0 +1,18 @@
+
+export function printAge(age){
+    console.log(age);
+}
+
+class CustomerDetails {
+
+    printFirstName(firstName){
+        console.log(firstName);
+    }
+    printLastName(lastName){
+        console.log(lastName);
+    }
+    
+
+
+}
+export const сustomerDetails = new CustomerDetails();
