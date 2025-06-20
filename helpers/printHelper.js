4779afe19504ec149b566ca4c7ee7f2931b665f9
Alexandr <muhabon93@gmail.com>
2025-06-20 17:27:43 +0300

    add params to method

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -8,6 +8,11 @@ class CustomerDetails {
     printFirstName(firstName){
         console.log(firstName);
     }
+
+    /**
+     * this method prints the last name of the customer
+     * @param {string} lastName 
+     */
     printLastName(lastName){
         console.log(lastName);
     }
